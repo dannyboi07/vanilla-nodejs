73a7a182750f0e85384a16eae5c440652ba52e9a
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-03-29 01:31:32 +0530

    Add files via upload

--- expnode.js
+++ expnode.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-const os = require("os");
-// const 
+const path = require("path");
+const busboy = require("busboy");
+const formidable = require("formidable");
 
-const hostname = "http://localhost"
 const PORT = 4000;
 // console.log(os.userInfo());
 // os.cpus().forEach(cpu => console.count(cpu.model))
@@ -12,8 +12,8 @@ const PORT = 4000;
 const server = http.createServer( async(req, res) => {
 
     const thisUrl = url.parse(req.url, true);
-    console.log(thisUrl.pathname);
-    console.log(1, thisUrl.pathname.match(/\/video\/[a-zA-Z]+\.mp4/));
+    console.log(thisUrl.pathname.split("/"));
+    console.log(1, thisUrl.pathname.match(/\/image\/[a-zA-Z0-9]\+.jpg/));
     if (thisUrl.pathname === "/home") {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html");
@@ -93,6 +93,7 @@ const server = http.createServer( async(req, res) => {
     }
     else if (req.url === "/readdir" && req.method === "GET"){
         fs.readdir("D:\\", (err, files) => {
+            if (err) throw(err);
             files.forEach((file, i) => {
                 res.write(file + "\n");
                 if (i === files.length - 1) {
@@ -103,16 +104,136 @@ const server = http.createServer( async(req, res) => {
     }
     else if (thisUrl.pathname === "/gethtml" && req.method === "GET") {
         fs.readFile("./index.html", "utf-8", (err, data) => {
+            if (err) throw(err);
             res.writeHead(200, { "Content-Type" : "text/html" });
             res.end(data);
         });
     }
     else if (thisUrl.pathname.match(/\/video\/[a-zA-Z]+\.mp4/i) && req.method === "GET") {
         const vidName = thisUrl.pathname.split("/")[2];
+        console.log("here",vidName);
+
+        res.setHeader("Content-Type", "video/mp4" );
+        // res.setHeader("Content-Encoding", "gzip");
+        // fs.createReadStream(vidName).pipe(zlib.createGzip()).pipe(res);
         const data = fs.createReadStream(vidName);
+        // data.pipe(zlib.createGzip()).pipe(res);
+        // data.on("data", chunk => res.write(chunk));
+        // data.on("end", () => res.end());
         data.pipe(res);
         // res.writeHead(200, { "Content-Type": "text/plain" }).end(vidName);
-    };
+    } 
+    else if (thisUrl.pathname === "/uploadbusboy") {
+
+        if (req.method === "POST") {
+            const bb = busboy({ headers: req.headers });
+
+            bb.on("file", (name, file, info) => {
+                const { filename, encoding, mimeType } = info;
+    
+                console.log(`File [${name}]: filename: ${filename}, encoding: ${encoding}, mimeType: ${mimeType}`);
+    
+                file.on("data", data => {
+                    console.log(`File [${name}] got ${data.length} bytes`);
+                }).on("close", () => console.log(`File [${name}] done`));
+            });
+    
+            bb.on("field", (name, val, info) => {
+                console.log(`Field [${name}]: value ${val}`);
+            }).on("close", () => {
+                console.log("Done parsing form");
+                res.writeHead(200, { Connection: "close", Location: "/" }).end();
+            });
+            req.pipe(bb);
+        }
+        else if (req.method === "GET") {
+            res.writeHead(200, { Connection: "close" });
+            res.end(`
+                <html>
+                    <head></head>
+                    <body>
+                        <form method="POST" enctype="multipart/form-data">
+                            <input type="file" name="fieldfield"></br>
+                            <input type="text" name="textfield"></br>
+                            <input type="submit">
+                        </form>
+                    </body>
+                </html>
+            `)
+        };
+        // const buffArray = []
+        // req.on("data", chunk => {
+        //     console.log(req.headers);
+        //         buffArray.push(chunk)
+        //         // console.log(Buffer.from((chunk)).toString());
+        // });
+        
+        // req.on("end", () => {
+        //     const resCon = Buffer.concat(buffArray).toLocaleString();
+        //     // console.log(resCon);
+        //     const resO = resCon.match(/Content-Disposition: form-data; name="image"; filename="(?:[^"]|"")*" Content-Type: image\/jpeg/)
+        //     console.log(resO);
+        //     res.statusCode = 200;
+        //     res.setHeader("Content-Type", "text/html").end(`<h1>OK ${resCon.match(/filename/)}</h1>`);
+        // });
+    }
+    else if (thisUrl.pathname === "/uploadformid") {
+        if (req.method === "GET") {
+            res.writeHead(200, { Connection: "close" });
+            res.end(`
+                <html>
+                    <head></head>
+                    <body>
+                        <form method="POST" action="http://localhost:4000/uploadformid" enctype="multipart/form-data">
+                            <input type="file" name="fieldfield"></br>
+                            <input type="text" name="textfield"></br>
+                            <input type="submit">
+                        </form>
+                    </body>
+                </html>
+            `)
+        }
+        else if (req.method === "POST") {
+            const options = {
+                multiples: false,
+                uploadDir: `${__dirname}/formidable-images`,
+                keepExtensions: true,
+                allowEmptyFiles: false,
+                filter: ({ name, originalFilename, mimetype }) => {
+                    return mimetype && mimetype.includes("image");
+                }
+            }
+
+            const form = formidable(options);
+
+            form.parse(req, (err, fields, file) => {
+                if (err) {
+                    res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+                    res.end(String(err));
+                    return;
+                }
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ fields, file }, null, 2));
+            });
+            return;
+        };
+    } else if (thisUrl.pathname === "/imagehtml" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type" : "text/html" });
+        res.write(`
+            <html>
+                <head></head>
+                <body>
+                    <img src="http://localhost:4000/image/a0306263869df9951c68d0c00.jpg" alt="image">
+                </body>
+            </html>
+        `);
+        res.end();
+    }
+    else if (thisUrl.pathname.match(/\/image\/[a-zA-Z0-9]+\.jpg/i) && req.method) {
+        const image = fs.createReadStream(`${__dirname}/formidable-images/${thisUrl.pathname.split("/")[2]}`);
+        image.on("error", err => console.error(err));
+        image.pipe(res);
+    }
 });
 
 server.listen(PORT, () => {
